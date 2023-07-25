d39a2f6e5a60089e53ae8f5a5ef6193233fe60d4
Noemipurr <noebeatrizm@gmail.com>
2023-07-25 13:22:14 -0300

    Colores en taiwind

---
+++ Burger-Planet/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'DarkBlue': '#170E3B',
+      'GreenNeon': '#37EC4B',
+      'DarkGreen': '#135868',
+      'Yellow': '#E3E6A9',
+      'Pink': '#D9B4B0',
+      'Blue': '#3F3EB3',
+      'GreenLight': '#7AF0A7'
+
+    },
+  },
+  plugins: [],
+
+}
+
+// #170E3B AZUL OSCURO
+// #37EC4B Verde NEON
+// #135868 Verde ocuro
+// #E3E6A9 Amarillo
+// #D9B4B0 Rosado
+// #3F3EB3 Azul 
+// #7AF0A7 verde agua
+
