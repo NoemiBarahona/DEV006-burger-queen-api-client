fff78805fc5ff4c1e0706d8fdb0c6495437def96
Noemipurr <noebeatrizm@gmail.com>
2023-07-25 11:57:44 -0300

    Primera vista sin CSS

---
+++ Burger-Planet/src/components/home/home.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import SelectProfile from "./selectProfile.jsx";
+
+function HomeProfile() {
+    return (
+        <div className="Home">
+            <SelectProfile />
+
+        </div>
+    )
+}
+
+export default HomeProfile;
+
+
+//   <>
+//   <>
+//     </>
+//     </>
+//     </>
+//     </>
+//     </>
+
+//   </>
+// </>
