c4d435aefa8c13f1a971bf8cb632993373a2ef68
Mika <mikamub@gmail.com>
2023-08-02 11:01:14 -0400

    lll

--- Burger-Planet/src/components/chefview/chefview.jsx
+++ Burger-Planet/src/components/chefview/chefview.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 // import React from 'react';
-import Img404 from './404.jsx'
+import ImagenDe404 from '../error/404.jsx'
 import 'tailwindcss/tailwind.css'
 
 function ChefView() {
     return (
         <>
             <div className="min-w-full bg-DarkBlue">
-                <Img404 />
+                <ImagenDe404 />
             </div>
         </>
     )
