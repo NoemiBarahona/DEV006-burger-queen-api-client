b365625f5f41749aef07aa20812d98013c9c6e90
Mika <mikamub@gmail.com>
2023-08-08 17:02:27 -0400

    error en useHistory

--- Burger-Planet/src/components/waiterview/tables.jsx
+++ Burger-Planet/src/components/waiterview/tables.jsx
@@ -2,47 +2,33 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
 import table from '../../assets/table.png';
+import { useHistory } from 'react-router-dom';
 
-function OrderTables() {
+function OrderTables(props) {
+    // eslint-disable-next-line react/prop-types
+    const { label, selected } = props;
+    const history = useHistory();
+
+    const handleTableClick = () => {
+        if (selected) {
+            history.push(`/table/${label}`); // Redireccionar a la vista de mesa con el nombre de la mesa en la URL
+        }
+    };
+
     return (
-        <div className="flex justify-center items-center m-auto">
-            <div className="grid grid-cols-3 gap-24 mt-32">
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 1
-                </div>
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 2
-                </div>
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 3
-                </div>
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 4
-                </div>
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 5
-                </div>
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 6
-                </div>
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 7
-                </div>
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 8
-                </div>
-                <div className="text-White w-38 h-38 flex flex-col items-center justify-center">
-                    <img src={table} alt="table" className="w-full h-full" />
-                    Mesa 9
-                </div>
+        <div
+            className={`flex justify-center items-center m-auto ${
+                selected ? 'border-2 border-blue-500 cursor-pointer' : 'cursor-not-allowed'
+            }`}
+            onClick={handleTableClick}
+        >
+            <div
+                className={`text-White w-38 h-38 flex flex-col items-center justify-center ${
+                    selected ? 'text-blue-500' : ''
+                }`}
+            >
+                <img src={table} alt="table" className="w-full h-full" />
+                {label}
             </div>
         </div>
     );
--- Burger-Planet/src/components/waiterview/waiterview.jsx
+++ Burger-Planet/src/components/waiterview/waiterview.jsx
@@ -1,18 +1,48 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import 'tailwindcss/tailwind.css'
+import React, { useState } from 'react';
+import 'tailwindcss/tailwind.css';
+import { useLocation, useHistory } from 'react-router-dom';
 import OrderTables from './tables.jsx';
 import Logocara from '../logopequeño';
+import WelcomeUser from '../adminview/welcome.jsx';
 
 function WaiterView() {
+    const location = useLocation();
+    const { role, email } = location.state || {};
+    const username = email ? email.split('@')[0] : '';
+    const history = useHistory();
+
+    const initialTables = [
+        { label: 'Mesa 1', selected: false },
+        { label: 'Mesa 2', selected: true },
+        // Agrega más mesas si es necesario
+    ];
+
+    const [tables, setTables] = useState(initialTables);
+
+    const handleTableClick = (index) => {
+        const updatedTables = tables.map((table, i) =>
+            i === index ? { ...table, selected: !table.selected } : table
+        );
+        setTables(updatedTables);
+    };
+
     return (
         <>
             <div className="min-w-full bg-DarkBlue">
                 <Logocara />
-                <OrderTables />
+                <WelcomeUser rol={role} user={username} />
+                {tables.map((table, index) => (
+                    <OrderTables
+                        key={index}
+                        label={table.label}
+                        selected={table.selected}
+                        onClick={() => handleTableClick(index)}
+                    />
+                ))}
             </div>
         </>
-    )
+    );
 }
 
 export default WaiterView;
