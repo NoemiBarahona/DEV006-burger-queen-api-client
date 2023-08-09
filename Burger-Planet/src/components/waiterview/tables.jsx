be5f21fe7677ecb44aaa25bf3b656826411926da
Mika <mikamub@gmail.com>
2023-08-09 11:33:22 -0400

    no funciona nada help

--- Burger-Planet/src/components/waiterview/tables.jsx
+++ Burger-Planet/src/components/waiterview/tables.jsx
@@ -2,31 +2,12 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
 import table from '../../assets/table.png';
-import { useHistory } from 'react-router-dom';
 
-function OrderTables(props) {
-    // eslint-disable-next-line react/prop-types
-    const { label, selected } = props;
-    const history = useHistory();
-
-    const handleTableClick = () => {
-        if (selected) {
-            history.push(`/table/${label}`); // Redireccionar a la vista de mesa con el nombre de la mesa en la URL
-        }
-    };
+function OrderTables(label, selected, onClick) {
 
     return (
-        <div
-            className={`flex justify-center items-center m-auto ${
-                selected ? 'border-2 border-blue-500 cursor-pointer' : 'cursor-not-allowed'
-            }`}
-            onClick={handleTableClick}
-        >
-            <div
-                className={`text-White w-38 h-38 flex flex-col items-center justify-center ${
-                    selected ? 'text-blue-500' : ''
-                }`}
-            >
+        <div className={`flex justify-center items-center m-auto ${selected ? 'border-2 border-blue-500' : ''}`}>
+            <div className={`text-White w-38 h-38 flex flex-col items-center justify-center ${selected ? 'text-blue-500' : ''}`} onClick={onClick}>
                 <img src={table} alt="table" className="w-full h-full" />
                 {label}
             </div>
@@ -35,4 +16,3 @@ function OrderTables(props) {
 }
 
 export default OrderTables;
-
